c44bab68266ee34cd3d70d38bd1df31a7331e0bf
Stevan <stevanpersonal@gmail.com>
2024-10-11 16:57:39 -0500

    refactor: deprecate astro support

--- app/src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = 'Metagallery';
-export const SITE_DESCRIPTION = 'Metagallery es una empresa especializada en experiencias 3D interactivas para museos, galerías y profesionales de cualquier rubro.';
