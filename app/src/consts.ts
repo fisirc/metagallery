76b5c332a196446db07f86201f7f8fd3097905de
Paolo Flores <pflores.fisi22@gmail.com>
2024-10-11 16:45:29 -0500

    add /app web-based 3D editor

---
+++ app/src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Metagallery';
+export const SITE_DESCRIPTION = 'Metagallery es una empresa especializada en experiencias 3D interactivas para museos, galerías y profesionales de cualquier rubro.';
