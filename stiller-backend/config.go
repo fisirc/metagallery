f2cfc670029f8c1342f0ebbb43c5c951aa4765ac
pandadiestro <destructo900@gmail.com>
2024-11-09 22:54:44 -0500

    feat: dynamic port selection

--- stiller-backend/config.go
+++ stiller-backend/config.go
@@ -4,6 +4,7 @@ import "github.com/profclems/go-dotenv"
 
 type ConfigType struct {
     // local directory where to retrieve/store data from/to
+    Port       string
     FilesPath  string
     DBPath     string
     Secret     []byte
@@ -14,6 +15,7 @@ func newConfig() *ConfigType {
     dotenv.Load()
 
     return &ConfigType{
+        Port: dotenv.GetString("Port"),
         FilesPath: dotenv.GetString("FilesPath"),
         DBPath: dotenv.GetString("DBPath"),
         Secret: []byte(dotenv.GetString("Secret")),
